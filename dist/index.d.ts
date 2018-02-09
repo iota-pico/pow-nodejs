138e5944b54d176cce2c02e834189fd1b6a1de22
Martyn Janes <martyn@obany.com>
2018-02-09 15:07:23 +0300

    Added index

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+/**
+ * Combined index of all the modules.
+ */
+export * from "./curlProofOfWork";
---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Combined index of all the modules.
+ */
+__export(require("./curlProofOfWork"));
