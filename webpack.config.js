0c19de4f69e8a3101e68159829b5ab4f610da920
Martyn Janes <martyn@obany.com>
2018-03-20 16:18:18 +0300

    Change module type to umd

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './pkg'),
         filename: pkgName + (isProd ? '.min' : '') + '.js',
-        libraryTarget: 'commonjs2',
+        libraryTarget: 'umd',
         libraryExport: undefined,
         library: {
             root: rootName,
